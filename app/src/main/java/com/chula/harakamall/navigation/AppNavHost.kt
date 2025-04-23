6ed2128b6def8b08356983d67783751cbb007edf
srstig <rayogutu36@gmailcom>
2025-04-23 13:30:34 +0300

    second commit

--- app/src/main/java/com/chula/harakamall/navigation/AppNavHost.kt
+++ app/src/main/java/com/chula/harakamall/navigation/AppNavHost.kt
@@ -1,12 +1,17 @@
 package com.chula.harakamall.navigation
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.chula.harakamall.data.UserDatabase
 import com.chula.harakamall.repository.UserRepository
 import com.chula.harakamall.ui.screens.about.AboutScreen
@@ -20,16 +25,23 @@ import com.chula.harakamall.ui.screens.form1.Form1Screen
 import com.chula.harakamall.ui.screens.home.HomeScreen
 import com.chula.harakamall.ui.screens.intent.IntentScreen
 import com.chula.harakamall.ui.screens.item.ItemScreen
+import com.chula.harakamall.ui.screens.products.AddProductScreen
+import com.chula.harakamall.ui.screens.products.EditProductScreen
+import com.chula.harakamall.ui.screens.products.ProductListScreen
 import com.chula.harakamall.ui.screens.service.ServiceScreen
 import com.chula.harakamall.ui.screens.splash.SplashScreen
 import com.chula.harakamall.ui.screens.start.StartScreen
 import com.chula.harakamall.viewmodel.AuthViewModel
+import com.chula.harakamall.viewmodel.ProductViewModel
 
+@RequiresApi(Build.VERSION_CODES.Q)
 @Composable
 fun AppNavHost(
     modifier: Modifier = Modifier,
     navController: NavHostController = rememberNavController(),
-    startDestination: String = ROUT_SPLASH
+    startDestination: String = ROUT_ADD_PRODUCT,
+    productViewModel: ProductViewModel= viewModel(),
+
 ) {
     val context = LocalContext.current
 
@@ -97,9 +109,31 @@ fun AppNavHost(
                 }
             }
         }
-
-
-
+        // PRODUCTS
+        composable(ROUT_ADD_PRODUCT) {
+            AddProductScreen(navController, productViewModel)
         }
+
+        composable(ROUT_PRODUCT_LIST) {
+            ProductListScreen(navController, productViewModel)
+        }
+
+        composable(
+            route = ROUT_EDIT_PRODUCT,
+            arguments = listOf(navArgument("productId") { type = NavType.IntType })
+        ) { backStackEntry ->
+            val productId = backStackEntry.arguments?.getInt("productId")
+            if (productId != null) {
+                EditProductScreen(productId, navController, productViewModel)
+            }
+        }
+
+
+
+
+
+
+
+    }
 }
 
